857771cdb4fa6ac20ae07692ec6477ef155962bb
Abhishek Singh <143773531+ABThakur123@users.noreply.github.com>
2026-09-17 17:38:03 +0530

    Add Feedback model class with properties and methods

---
+++ moodmeal/src/main/java/com/moodmeal/model/Feedback.java
@@ -0,0 +1,14 @@
+package com.moodmeal.model;
+
+public class Feedback {
+    private String email;
+    private int rating;
+    private String comment;
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public int getRating() { return rating; }
+    public void setRating(int rating) { this.rating = rating; }
+    public String getComment() { return comment; }
+    public void setComment(String comment) { this.comment = comment; }
+}
