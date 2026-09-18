38d25ff31aac65e9e7a87ed0b82ceccd744f30ed
Abhishek Singh <143773531+ABThakur123@users.noreply.github.com>
2026-09-18 20:22:08 +0530

    Add JPA annotations to User model

--- moodmeal/src/main/java/com/moodmeal/model/User.java
+++ moodmeal/src/main/java/com/moodmeal/model/User.java
@@ -1,10 +1,34 @@
 package com.moodmeal.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String username;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
     private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getUsername() {
         return username;
